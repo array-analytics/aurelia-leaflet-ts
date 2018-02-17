55097c4f52491bc24e0b31d7669f9bc8656debcd
diegohb <>
2018-02-16 22:06:37 -0500

    -added aliases for leaflet namespaced definitions -updated typings for leaflet

--- custom_typings/leaflet.d.ts
+++ custom_typings/leaflet.d.ts
@@ -1,5 +1,19 @@
-export namespace L {
-    class Map {
-        setView(center: LatLngExpression): this;
-    }
+import * as L from "leaflet";
+
+module "leaflet" {
+
+    export type LayersObject = L.Control.LayersObject;
+
+    export type LayersControl = L.Control.Layers;
+
+    export type ScaleControl = L.Control.Scale;
+
+    export let createLayersControl = L.control.layers;
+
+    export let createScaleControl = L.control.scale;
+
 }
+
+export type LeafLayer = L.Layer & { id?: string, url?: string };
+
+
