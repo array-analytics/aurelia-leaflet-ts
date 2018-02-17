a4b5b9bbd9cec34d4d15451b1a57c06baa744202
diegohb <>
2018-02-17 01:03:15 -0500

    something is up with the index file, i gotta figure it out.. either i get a trace error at consuming app or no trace error but runtime 404 looking for "au-leaflet"

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { FrameworkConfiguration } from "aurelia-framework";
-import { AULeafletCustomElement } from "./au-leaflet";
 
 export function configure(aurelia: FrameworkConfiguration) {
-    //use it so tsc to AMD format won't complain.
-    AULeafletCustomElement;
     aurelia.globalResources([
         "./au-leaflet"
     ]);
