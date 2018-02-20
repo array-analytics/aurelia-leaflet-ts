a737eac7da82d01e44d7ee1f41e1e1ff89824ccb
diegohb <>
2018-02-20 17:39:59 -0500

    export all types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { FrameworkConfiguration } from "aurelia-framework";
 import { AULeafletCustomElement } from "./au-leaflet";
 
+export * from "./au-leaflet";
+export * from "./layer-factory";
+export * from "./au-leaflet-exception";
+
 export function configure(aurelia: FrameworkConfiguration) {
     //use it so tsc to AMD format won't complain.
     if (AULeafletCustomElement) { }
